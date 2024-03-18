ac47d9fff46df010d8a7bcbc334ebf48bddf29ad
ult-sohtome <a.sohtome@ultinet.co.jp>
2024-03-18 11:31:43 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+/**
 function fib(n){
   if (n===0){
     return 0;
@@ -11,4 +12,20 @@ const length = 40;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
+ */
 
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+function fib(n){
+  if(memo.has(n)){
+    return memo.get(n);
+  }
+  const value =fib(n-1)+fib(n-2);
+  memo.set(n, value);
+  return value;
+}
+const length = 40;
+for(let i = 0; i <= length; i++){
+  console.log(fib(i));
+}
